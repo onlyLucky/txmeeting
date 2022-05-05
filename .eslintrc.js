8949fc4047345fae78df74cd2a7cd3576e30df5d
feynman <2901954062@qq.com>
2022-05-05 16:51:28 +0800

    chore: eslint config add

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+/*
+ * @Author: fg
+ * @Date: 2022-04-14 10:21:44
+ * @LastEditors: fg
+ * @LastEditTime: 2022-05-05 16:24:30
+ * @Description: content
+ */
+module.exports = {
+  extends: [
+    'standard'
+  ]
+}
