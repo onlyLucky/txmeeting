23c2fbc31f0b72f487548ee668a3046ebc235865
feynman <2901954062@qq.com>
2022-05-06 17:19:25 +0800

    feat: 逻辑代码新增

--- src/inputPage.js
+++ src/inputPage.js
@@ -1,19 +1,59 @@
+
 /*
  * @Author: fg
  * @Date: 2022-05-06 13:39:52
  * @LastEditors: fg
- * @LastEditTime: 2022-05-06 14:05:05
- * @Description: content
+ * @LastEditTime: 2022-05-06 17:04:31
+ * @Description: InputPage
  */
-
+import Config from '../public/config'
 const InputPage = {
   init () {
-    document.getElementById('confirmBtn').onclick = handleBtnTap
+    const that = this
+    document.getElementById('confirmBtn').onclick = this.handleBtnTap
+    $('#Input').bind('input propertychange', function (event) {
+      that.data.inputValue = $(this).val()
+    })
+  },
+  data: {
+    showInput: true,
+    inputValue: ''
+  },
+  handleBtnTap () {
+    if (InputPage.data.inputValue) {
+      InputPage.getUrl()
+    }
+  },
+  getUrl () {
+    const url = `${Config.apiUrl}/notifyMembership?userName=${this.data.inputValue}`
+    console.log(url)
+    $.ajax({
+      type: 'post',
+      url,
+      success: (res) => { // 【成功回调】
+        if (res.code === 0) {
+          if (res.msg) {
+            window.location.href = res.msg
+          } else {
+            this.showMsg('当前暂无会议')
+          }
+        } else {
+          this.showMsg('加入会议失败')
+        }
+      },
+      error: function (xhr, type) { // 【失败回调】
+        this.showMsg('加入会议失败')
+        console.log(xhr, type, 'baocuo')
+      }
+    })
+  },
+  showMsg (txt) {
+    $('#alert').text(txt)
+    $('#alert').css({ bottom: '1rem' })
+    setTimeout(() => {
+      $('#alert').css({ bottom: '-10rem' })
+    }, 2000)
   }
-}
-
-function handleBtnTap () {
-  console.log('12321')
 }
 
 export default InputPage
--- src/main.js
+++ src/main.js
@@ -2,14 +2,16 @@
  * @Author: fg
  * @Date: 2022-05-06 10:28:25
  * @LastEditors: fg
- * @LastEditTime: 2022-05-06 14:04:13
+ * @LastEditTime: 2022-05-06 15:14:46
  * @Description: 主入口
  */
-import './libs/rem.js'
+import resizeRem from './libs/rem.js'
 import './style/index.css'
+import './libs/jquery.min.js'
 import InputPage from './inputPage.js'
 console.log('src/index')
 
 window.onload = function () {
   InputPage.init()
+  resizeRem()
 }
