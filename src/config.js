2b432eff277e11f09c71b6a99b96f9b87de55931
feynman <2901954062@qq.com>
2022-05-06 14:22:07 +0800

    ci: 新增全局配置文件

---
+++ src/config.js
@@ -0,0 +1,13 @@
+/*
+ * @Author: fg
+ * @Date: 2022-05-06 14:14:19
+ * @LastEditors: fg
+ * @LastEditTime: 2022-05-06 14:15:33
+ * @Description: config 配置
+ */
+
+const Config = {
+  apiUrl: ''
+}
+
+export default Config
