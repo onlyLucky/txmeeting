6d9d29d61e1d5e39f272eaea15f4f24a1a1fc0a4
feynman <2901954062@qq.com>
2022-05-06 14:07:13 +0800

    wip: inputpage 页面逻辑初始化添加

---
+++ src/inputPage.js
@@ -0,0 +1,19 @@
+/*
+ * @Author: fg
+ * @Date: 2022-05-06 13:39:52
+ * @LastEditors: fg
+ * @LastEditTime: 2022-05-06 14:05:05
+ * @Description: content
+ */
+
+const InputPage = {
+  init () {
+    document.getElementById('confirmBtn').onclick = handleBtnTap
+  }
+}
+
+function handleBtnTap () {
+  console.log('12321')
+}
+
+export default InputPage
--- src/main.js
+++ src/main.js
@@ -2,9 +2,14 @@
  * @Author: fg
  * @Date: 2022-05-06 10:28:25
  * @LastEditors: fg
- * @LastEditTime: 2022-05-06 10:30:01
+ * @LastEditTime: 2022-05-06 14:04:13
  * @Description: 主入口
  */
 import './libs/rem.js'
 import './style/index.css'
+import InputPage from './inputPage.js'
 console.log('src/index')
+
+window.onload = function () {
+  InputPage.init()
+}
