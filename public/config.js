19cab3a2babfac466803fc1441324214d2e50a98
feynman <2901954062@qq.com>
2022-05-06 17:20:49 +0800

    refactor: config全局配置文件目录重构，mock数据清除

--- public/config.js
+++ public/config.js
@@ -2,12 +2,12 @@
  * @Author: fg
  * @Date: 2022-05-06 14:14:19
  * @LastEditors: fg
- * @LastEditTime: 2022-05-06 14:15:33
+ * @LastEditTime: 2022-05-06 16:07:04
  * @Description: config 配置
  */
 
 const Config = {
-  apiUrl: ''
+  apiUrl: 'http://192.168.19.89:8071/client'
 }
 
 export default Config
