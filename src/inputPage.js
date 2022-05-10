609bd0f54232d95996ec999b51f33a1fa2e79fa5
feynman <2901954062@qq.com>
2022-05-10 14:34:01 +0800

    feat: 页面绑定添加本地储存，登陆检测本地存储是否存在，自动登陆

--- src/inputPage.js
+++ src/inputPage.js
@@ -3,7 +3,7 @@
  * @Author: fg
  * @Date: 2022-05-06 13:39:52
  * @LastEditors: fg
- * @LastEditTime: 2022-05-09 14:15:23
+ * @LastEditTime: 2022-05-10 14:30:41
  * @Description: InputPage
  */
 import { getUrlParam } from './libs/utools'
@@ -16,6 +16,7 @@ const InputPage = {
     //   console.log(res, 123)
     // })
     console.log(getUrlParam('mac'))
+    $('.result').hide()
     $('#Input').bind('input propertychange', function (event) {
       that.data.inputValue = $(this).val()
     })
@@ -26,6 +27,11 @@ const InputPage = {
         document.documentElement.scrollTop = 0
       }
     })
+    if (getUrlParam('mac')) {
+      that.data.isBind = true
+    } else {
+      that.data.isBind = false
+    }
     $('#Input').on('focusout', function () {
       const ua = window.navigator.userAgent
       if (ua.indexOf('iPhone') > 0 || ua.indexOf('iPad') > 0) { // 键盘收起页面空白问题
@@ -33,25 +39,37 @@ const InputPage = {
         document.documentElement.scrollTop = 0
       }
     })
+    if (window.localStorage.getItem('username')) {
+      if (getUrlParam('meetingId')) {
+        this.postMeet({
+          url: `/joinMeeting?userName=${window.localStorage.getItem('username')}&meetingId=${Number(getUrlParam('meetingId'))}`
+        })
+      } else {
+        this.data.inputValue = window.localStorage.getItem('username')
+        $('#Input').val(window.localStorage.getItem('username'))
+      }
+    }
   },
   data: {
     showInput: true,
-    inputValue: ''
+    inputValue: '',
+    isBind: false
   },
   handleBtnTap () {
     if (InputPage.data.inputValue) {
       let url = ''
       if (getUrlParam('mac')) {
-        url = `/bindPad?userName=${this.data.inputValue}&mac=${getUrlParam('mac')}`
+        url = `/bindPad?userName=${InputPage.data.inputValue}&mac=${getUrlParam('mac')}`
       }
       if (getUrlParam('meetingId')) {
-        url = `/joinMeeting?userName=${this.data.inputValue}&meetingId=${Number(getUrlParam('meetingId'))}`
+        url = `/joinMeeting?userName=${InputPage.data.inputValue}&meetingId=${Number(getUrlParam('meetingId'))}`
       }
       if (url) {
-        InputPage.getUrl({ url })
+        InputPage.postMeet({ url })
       }
     }
   },
+
   postMeet (httpConfig) {
     const url = `${Config.apiUrl}${httpConfig.url}`
     $.ajax({
@@ -60,16 +78,27 @@ const InputPage = {
       success: (res) => { // 【成功回调】
         if (res.code === 0) {
           if (res.msg) {
-            window.location.href = res.msg
+            if (this.data.isBind) {
+              window.localStorage.setItem('username', res.msg)
+              this.showMsg('会议绑定成功')
+              $('.result span').text(res.msg)
+              $('.inputBox').hide()
+              $('.bottomBtn').hide()
+              $('.result').show()
+            } else {
+              window.location.href = res.msg
+            }
           } else {
             this.showMsg('当前暂无会议')
           }
         } else {
-          this.showMsg('加入会议失败')
+          const txt = this.data.isBind ? '会议绑定失败' : '加入会议失败'
+          this.showMsg(res.msg || txt)
         }
       },
       error: function (xhr, type) { // 【失败回调】
-        this.showMsg('加入会议失败')
+        const txt = this.data.isBind ? '会议绑定失败' : '加入会议失败'
+        this.showMsg(txt)
         console.log(xhr, type, 'baocuo')
       }
     })
